4849feaa2715bacd0adc66ac94e04c7aa8da03cd
Saulo Almeida <saulohenrique786@gmail.com>
2022-08-11 09:47:56 -0300

    feat: add models

---
+++ src/SavePassword.API/Application/Model/Common/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SavePassword.API.Application.Model.Common
+{
+    public abstract class BaseModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdateAt { get; set; }
+        public bool Deleted { get; private set; } = false;
+
+        public void Delete() => Deleted = true;
+    }
+}
---
+++ src/SavePassword.API/Application/Model/Secret.cs
@@ -0,0 +1,22 @@
+﻿using SavePassword.API.Application.Model.Common;
+
+namespace SavePassword.API.Application.Model
+{
+    public class Secret : BaseModel
+    {
+        public string Name { get; set; }
+        public string? IconUrl { get; set; }
+        public string? WebSite { get; set; }
+        public IList<Account> Accounts { get; set; } = new List<Account>(); 
+        public string? Note { get; set; }
+
+        public void AddAccount(Account acc) => Accounts.Add(acc);
+
+        public void RemoveAccount(Guid id)
+        {
+            var account = Accounts.FirstOrDefault(x => x.Id == id);
+
+            if (account != null) account.Delete();
+        }
+    }
+}
