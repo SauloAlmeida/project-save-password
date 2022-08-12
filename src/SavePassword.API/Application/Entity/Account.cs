fd4fadb8faa21fbcfaf1d5d472e5aeb59ea52747
Saulo Almeida <saulohenrique786@gmail.com>
2022-08-12 09:00:31 -0300

    refactor: model to entity

---
+++ src/SavePassword.API/Application/Entity/Account.cs
@@ -0,0 +1,12 @@
+﻿using SavePassword.API.Application.Entity.Common;
+
+namespace SavePassword.API.Application.Entity
+{
+    public class Account : BaseModel
+    {
+        public string? NickName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string? Note { get; set; }
+    }
+}
