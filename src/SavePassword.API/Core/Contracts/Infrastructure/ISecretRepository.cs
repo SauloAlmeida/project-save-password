69d3c8bc840a3fe56f06f6af211888d9e911e52e
Saulo Almeida <saulohenrique786@gmail.com>
2022-08-12 09:06:26 -0300

    feat: repository contract

---
+++ src/SavePassword.API/Core/Contracts/Infrastructure/ISecretRepository.cs
@@ -0,0 +1,14 @@
+﻿using SavePassword.API.Application.DTO.InputModel;
+using SavePassword.API.Application.Entity;
+
+namespace SavePassword.API.Core.Contracts.Infrastructure
+{
+    public interface ISecretRepository
+    {
+        Task<IList<Secret>> GetAllAsync(CancellationToken ct);
+        Task<Secret> GetAsync(Guid id);
+        Task AddAsync(SecretInputModel dto, CancellationToken ct);
+        Task UpdateAsync(Guid id, SecretInputModel dto, CancellationToken ct);
+        Task DeleteAsync(Guid id);   
+    }
+}
